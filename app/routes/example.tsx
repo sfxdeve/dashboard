46cf0bd02c3724715aa6b33878badb4757f2d59f
Shayan Fareed <sfx.deve@gmail.com>
2026-02-24 11:27:13 +0500

    refactor: update routing to use Example component and remove Home component

---
+++ app/routes/example.tsx
@@ -0,0 +1,12 @@
+import type { Route } from './+types/example';
+
+export function meta({ }: Route.MetaArgs) {
+  return [
+    { title: 'Example Page' },
+    { name: 'description', content: 'Welcome to the example page.' },
+  ];
+}
+
+export default function Example() {
+  return <main></main>;
+}
